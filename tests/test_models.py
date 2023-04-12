02ca42ff49af38181190462bc63b2ee9f49e6408
Andy Tebbs <andy.tebbs@ukaea.uk>
2023-04-12 15:11:44 +0100

    updating test and adding yaml continuous integration testing

--- tests/test_models.py
+++ tests/test_models.py
@@ -8,7 +8,7 @@ import pytest
     [
         ([[0, 0], [0, 0], [0, 0]], [0, 0]),
         ([[1, 2], [3, 4], [5, 6]], [3, 4]),
-    ])
+    ],ids=("firsttestcase", "secondtestcase"))
 def test_daily_mean(test, expected):
     """Test mean function works for array of zeroes and positive integers."""
     from inflammation.models import daily_mean
