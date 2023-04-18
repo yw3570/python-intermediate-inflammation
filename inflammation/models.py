05f3228d6c1b89743bad0d75049637dd6e5dd07b
Andy Tebbs <andy.tebbs@ukaea.uk>
2023-04-18 13:25:46 +0100

    update on Tuesday

--- inflammation/models.py
+++ inflammation/models.py
@@ -37,3 +37,19 @@ def daily_min(data):
 @dataclasses.dataclass
 class Patient:
     name: str
+
+
+def patient_normalise(data):
+    """
+    Normalise patient data from a 2D inflammation data array.
+
+    NaN values are ignored, and normalised to 0.
+
+    Negative values are rounded to 0.
+    """
+    maxima = np.nanmax(data, axis=1)
+    with np.errstate(invalid='ignore', divide='ignore'):
+        normalised = data / maxima[:, np.newaxis]
+    normalised[np.isnan(normalised)] = 0
+    normalised[normalised < 0] = 0
+    return normalised
